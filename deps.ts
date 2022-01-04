82e7057531dd0055777b7acc62ef932db151bbbd
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-04 14:14:46 +0900

    :sparkles: Throw Error when environmental variables are missing

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 import "https://deno.land/x/dotenv/load.ts";
 export * from "https://pax.deno.dev/takker99/scrapbox-userscript-std@0.1.0/rest/page-data.ts";
+export { ensureString } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { exportPages, importPages } from "./deps.ts";
+import { ensureString, exportPages, importPages } from "./deps.ts";
 
 const sid = Deno.env.get("SID");
 const exportingProjectName = Deno.env.get("SOURCE_PROJECT_NAME"); //インポート元(本来はprivateプロジェクト)
@@ -6,48 +6,45 @@ const importingProjectName = Deno.env.get("DESTINATION_PROJECT_NAME"); //イン�
 const shouldDuplicateByDefault =
   Deno.env.get("SHOULD_DUPLICATE_BY_DEFAULT") === "True";
 
-if (
-  sid !== undefined && exportingProjectName !== undefined &&
-  importingProjectName !== undefined
-) {
-  console.log(`Exporting a json file from "/${exportingProjectName}"...`);
-  const result = await exportPages(exportingProjectName, {
-    sid,
-    metadata: false,
-  });
-  if (!result.ok) {
-    console.error(`Export Error[${result.name}]:\n\t${result.message}`);
-  } else {
-    const { pages } = result;
-    console.log("exported: ", pages);
+ensureString(sid);
+ensureString(exportingProjectName);
+ensureString(importingProjectName);
 
-    const importingPages = pages.filter(({ lines }) => {
-      if (lines.some((line) => line.includes("[private.icon]"))) {
-        return false;
-      } else if (lines.some((line) => line.includes("[public.icon]"))) {
-        return true;
-      } else {
-        return shouldDuplicateByDefault;
-      }
-    });
-    if (importingPages.length > 0) {
-      console.log(
-        `Importing ${importingPages.length} pages to "/${importingProjectName}"...`,
-      );
-      const result = await importPages(importingProjectName, {
-        pages: importingPages,
-      }, {
-        sid,
-      });
-      if (result.ok) {
-        console.log(result.message);
-      } else {
-        console.error(`Import Error[${result.name}]:\n\t${result.message}`);
-      }
-    } else {
-      console.log("No page to be imported found.");
-    }
-  }
+console.log(`Exporting a json file from "/${exportingProjectName}"...`);
+const result = await exportPages(exportingProjectName, {
+  sid,
+  metadata: false,
+});
+if (!result.ok) {
+  console.error(`Export Error[${result.name}]:\n\t${result.message}`);
 } else {
-  console.log("Environmental variables are missing.");
+  const { pages } = result;
+  console.log("exported: ", pages);
+
+  const importingPages = pages.filter(({ lines }) => {
+    if (lines.some((line) => line.includes("[private.icon]"))) {
+      return false;
+    } else if (lines.some((line) => line.includes("[public.icon]"))) {
+      return true;
+    } else {
+      return shouldDuplicateByDefault;
+    }
+  });
+  if (importingPages.length > 0) {
+    console.log(
+      `Importing ${importingPages.length} pages to "/${importingProjectName}"...`,
+    );
+    const result = await importPages(importingProjectName, {
+      pages: importingPages,
+    }, {
+      sid,
+    });
+    if (result.ok) {
+      console.log(result.message);
+    } else {
+      console.error(`Import Error[${result.name}]:\n\t${result.message}`);
+    }
+  } else {
+    console.log("No page to be imported found.");
+  }
 }
