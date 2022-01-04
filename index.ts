24a72b806ee157592fb01dbf3aca27160e000b35
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-04 15:05:37 +0900

    :+1: 例外を全てcatchしてconsoleに写す

--- index.ts
+++ index.ts
@@ -11,40 +11,44 @@ ensureString(exportingProjectName);
 ensureString(importingProjectName);
 
 console.log(`Exporting a json file from "/${exportingProjectName}"...`);
-const result = await exportPages(exportingProjectName, {
-  sid,
-  metadata: true,
-});
-if (!result.ok) {
-  console.error(`Export Error[${result.name}]:\n\t${result.message}`);
-} else {
-  const { pages } = result;
-  console.log("exported: ", pages);
-
-  const importingPages = pages.filter(({ lines }) => {
-    if (lines.some((line) => line.text.includes("[private.icon]"))) {
-      return false;
-    } else if (lines.some((line) => line.text.includes("[public.icon]"))) {
-      return true;
-    } else {
-      return shouldDuplicateByDefault;
-    }
+try {
+  const result = await exportPages(exportingProjectName, {
+    sid,
+    metadata: true,
   });
-  if (importingPages.length > 0) {
-    console.log(
-      `Importing ${importingPages.length} pages to "/${importingProjectName}"...`,
-    );
-    const result = await importPages(importingProjectName, {
-      pages: importingPages,
-    }, {
-      sid,
-    });
-    if (result.ok) {
-      console.log(result.message);
-    } else {
-      console.error(`Import Error[${result.name}]:\n\t${result.message}`);
-    }
+  if (!result.ok) {
+    console.error(`Export Error[${result.name}]:\n\t${result.message}`);
   } else {
-    console.log("No page to be imported found.");
+    const { pages } = result;
+    console.log("exported: ", pages);
+
+    const importingPages = pages.filter(({ lines }) => {
+      if (lines.some((line) => line.text.includes("[private.icon]"))) {
+        return false;
+      } else if (lines.some((line) => line.text.includes("[public.icon]"))) {
+        return true;
+      } else {
+        return shouldDuplicateByDefault;
+      }
+    });
+    if (importingPages.length > 0) {
+      console.log(
+        `Importing ${importingPages.length} pages to "/${importingProjectName}"...`,
+      );
+      const result = await importPages(importingProjectName, {
+        pages: importingPages,
+      }, {
+        sid,
+      });
+      if (result.ok) {
+        console.log(result.message);
+      } else {
+        console.error(`Import Error[${result.name}]:\n\t${result.message}`);
+      }
+    } else {
+      console.log("No page to be imported found.");
+    }
   }
+} catch (e) {
+  console.error(e);
 }
